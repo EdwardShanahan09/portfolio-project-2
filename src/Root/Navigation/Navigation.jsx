e39969dfbc6d578f4571702bbbd9c13a72439c27
Edward Shanahan <edwardshanahan79@gmail.com>
2024-06-11 17:42:54 +0100

    Style navigation

--- src/Root/Navigation/Navigation.jsx
+++ src/Root/Navigation/Navigation.jsx
@@ -5,28 +5,28 @@ import AllTodosIcon from "../../assets/icons/all-todos-icon.svg";
 import TasksIcon from "../../assets/icons/tasks-icon.svg";
 const Navigation = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar h-full bg-primary flex justify-between items-center direction-col flex-col p-4 rounded-r-3xl">
       <Link to="profile">
-        <img src={ProfileIcon} alt="Profile Icon" />
+        <img className="w-8" src={ProfileIcon} alt="Profile Icon" />
         <p>John Doe</p>
       </Link>
 
       <ul className="navbar__list">
-        <li className="navbar__item">
-          <Link to="today-tasks">
-            <img src={TodaysTasksIcon} alt="Today Tasks Icon" />
+        <li className="flex">
+          <Link className="flex" to="today-tasks">
+            <img className="w-6" src={TodaysTasksIcon} alt="Today Tasks Icon" />
             <span>Today Tasks</span>
           </Link>
         </li>
         <li>
-          <Link to="all-todos">
-            <img src={AllTodosIcon} alt="Today Tasks Icon" />
+          <Link className="flex" to="all-todos">
+            <img className="w-6" src={AllTodosIcon} alt="Today Tasks Icon" />
             <span>All Todos</span>
           </Link>
         </li>
         <li>
-          <Link to="tasks">
-            <img src={TasksIcon} alt="Tasks Icon" />
+          <Link className="flex" to="tasks">
+            <img className="w-6" src={TasksIcon} alt="Tasks Icon" />
             <span>Tasks</span>
           </Link>
         </li>
--- src/Root/RootLayout.jsx
+++ src/Root/RootLayout.jsx
@@ -3,11 +3,11 @@ import Navigation from "./Navigation/Navigation";
 
 const RootLayout = () => {
   return (
-    <div className="grid grid-rows-1 lg:grid-cols-4 min-h-screen">
+    <div className="grid grid-rows-1 lg:grid-cols-6 min-h-screen">
       <div className="lg:col-span-1 order-last lg:order-first">
         <Navigation />
       </div>
-      <div className="lg:col-span-3">
+      <div className="lg:col-span-5">
         <Outlet />
       </div>
     </div>
