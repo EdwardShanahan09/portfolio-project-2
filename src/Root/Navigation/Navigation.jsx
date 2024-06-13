eb8bc5671d07c5fb0e0b286e6b0d1dd216b6ce21
Edward Shanahan <edwardshanahan79@gmail.com>
2024-06-13 20:25:00 +0100

    Fix profile styling

--- src/Root/Navigation/Navigation.jsx
+++ src/Root/Navigation/Navigation.jsx
@@ -7,12 +7,9 @@ import SignOutIcon from "../../assets/icons/sign-out-icon.svg";
 
 const Navigation = () => {
   return (
-    <nav className="navbar h-full bg-dark flex flex-col items-center lg:justify-between p-2 rounded-t-3xl lg:rounded-r-3xl lg:rounded-t-none">
-      <Link
-        to="profile"
-        className="flex items-center justify-center w-full lg:w-auto mb-4 lg:mb-0"
-      >
-        <img className="w-8" src={ProfileIcon} alt="Profile Icon" />
+    <nav className="navbar h-full bg-dark flex lg:flex-col items-center justify-between p-6 lg:px-0">
+      <Link to="profile" className="flex flex-col items-center">
+        <img className="w-6 lg:w-8 mb-2" src={ProfileIcon} alt="Profile Icon" />
         <p className="text-light hidden lg:inline ml-2">John Doe</p>
       </Link>
 
